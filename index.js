a1ecdf7fa81ff33777445cada6d69214ff6210c0
livrbecca <69110329+livrbecca@users.noreply.github.com>
2020-11-06 16:01:20 +0000

    my first server

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require ('body-parser')
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const liv = {
+const oliviaProfile = {
   firstName: "Olivia",
   nickName: "is nicki, but my name aint nicole",
   lastName: "Walker",
@@ -12,6 +14,8 @@ const liv = {
 
 const facts = [
   "I like Ed Sheeran",
+  "I love skincare",
+  "Coding is hard but fun!",
   "I love apple crumble",
   "My favourite soap is Eastenders",
   "I dont like peanut butter",
@@ -26,43 +30,180 @@ const facts = [
   "I went to Lancaster University",
   "I have a law degree",
   "I had a hamster called Muffin",
-  "I live in Wolverhampton"
+  "I live in Wolverhampton",
 ];
 
+const livsProfile = {
+  firstName: "Liv",
+  lastName: "Walker",
+  preferences: {
+    foods: ["patties", "ramen"],
+    colour: "yellow",
+    number: 11,
+  },
+  hoursOfSleep: 8.5,
+};
 
+const randomProfile = {
+  firstName: "Black",
+  lastName: "Codher",
+  preferences: {
+    coding: true,
+    languages: ["HTML", "Java", "React", "Python"],
+    color: ["black", "orange"],
+  },
+  hoursOfSleep: 0,
+};
 
-// randomItem = liv[Math.floor(Math.random() * liv.length)];
+const db = {
+  profiles: {
+    1000: livsProfile,
+    2000: randomProfile,
+    3000: oliviaProfile,
+  },
+  anime: {
+    0: {
+      title: "Gintama",
+      bestCharacter: "Hijikata",
+    },
+    1: {
+      title: "Fairy Tail",
+      bestCharacter: "Erza Scarlett",
+    },
+    2: {
+      title: "Naruto",
+      bestCharacter: "No one.",
+    },
+  },
+};
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json())
+// GET ROUTES COMPLETE
 
-const vineQuotes = [
-  "Shrek is love, Shrek is life",
-  "AGH STOP! I could've droped my croissant!",
-  "Road work ahead? uh YEAH, I sure hope it does?!",
-  "thats my OPINIONNNNNN",
-  "Girl, you're thicker than a bowl of oatmeal",
-  "Do it for the vine! I ain't gone do it!",
-  "Whats 9 + 10? ........ 21???"
-]
-
-app.get("/", (req, res) => {
-  res.json({ message: "Livs local server is running, woohoo" });
-});
-
-app.get("/vine-quotes", (req, res) => {
-  res.json({quotes: vineQuotes})
-});
-
-app.post("/vine-quotes", (req, res) => {
- 
-});
-
-app.get("/about-me", (req, res) => {
+app.get("/profiles", (req, res) => {
   const randomIndex = Math.floor(Math.random() * facts.length);
   const randomFact = facts[randomIndex];
-  res.json({...liv, fact: randomFact});
+
+  res.status(200).json({
+    ...oliviaProfile,
+    fact: randomFact,
+    status: "success",
+    data: db.profiles,
+  });
+});
+
+app.get("/profiles/:userId", (req, res) => {
+  console.log(req.params.userId);
+
+  const matchingProfile = db.profiles[req.params.userId];
+
+  if (matchingProfile) {
+    res.json({
+      status: "success",
+      data: matchingProfile,
+    });
+  } else {
+    res.status(404).json({
+      message: "Couldn't find user with that id. Please try again",
+    });
+  }
+});
+
+app.get("/anime", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: db.anime,
+  });
+});
+
+app.get("/anime/:animeId", (req, res) => {
+  console.log(req.params.animeId);
+
+  const matchingAnime = db.anime[req.params.animeId];
+
+  if (matchingAnime) {
+    res.json({
+      status: "success",
+      data: matchingAnime,
+    });
+  } else {
+    res.status(404).json({
+      message: "Couldn't find anime data. Please try again",
+    });
+  }
+});
+
+// POST ROUTES COMPLETE
+
+app.post("/profiles", (req, res) => {
+  const existingIds = Object.keys(db.profiles);
+  const largestKey = Math.max(...existingIds);
+
+  const newKey = largestKey + 1;
+
+  db.profiles[newKey] = req.body;
+
+  res.status(201).json({
+    status: "success",
+    message: `Created a profile with id of ${newKey}`,
+  });
+});
+
+app.post("/anime", (req, res) => {
+  const existingIds = Object.keys(db.anime);
+  const largestKey = Math.max(...existingIds);
+
+  const newKey = largestKey + 1;
+
+  db.anime[newKey] = req.body;
+
+  res.status(201).json({
+    status: "success",
+    message: `Added a anime with id of ${newKey}`,
+  });
+});
+
+// PATCH ROUTES
+
+app.patch("/profiles/:userId", (req, res) => {
+  db.profiles[req.params.userId] = {
+    ...db.profiles[req.params.userId],
+    ...req.body,
+  };
+
+  res.status(200).json({
+    message: "User updated",
+  });
+});
+
+app.patch("/anime/:animeId", (req, res) => {
+  db.anime[req.params.animeId] = {
+    ...db.anime[req.params.animeId],
+    ...req.body,
+  };
+
+  res.status(200).json({
+    message: "Anime updated",
+  });
+});
+
+// DELETE ROUTES
+
+app.delete("/profiles/:userId", (req, res) => {
+  delete db.profiles[req.params.userId];
+
+  res.status(200).json({
+    status: "success",
+    message: "Profile deleted ",
+  });
+});
+
+app.delete("/anime/:animeId", (req, res) => {
+  delete db.anime[req.params.animeId];
+
+  res.status(200).json({
+    status: "success",
+    message: "Anime deleted",
+  });
 });
 
 app.listen(4000, () => {
